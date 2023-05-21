b3b3850eca92b11ab2a080db3caca96ee173c771
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-05-21 18:38:38 +0200

    Create client endpoints

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,32 @@
+import { Routes, Route, Outlet } from 'react-router-dom'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+const Layout = () => {
+	return (
+		<div>
+			<Navbar />
+			<Outlet />
+			<Footer />
+		</div>
+	)
+}
+
 const App = () => {
-	return <div>App</div>
+	return (
+		<div>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					<Route path='/' element={<Home />} />
+					<Route path='/products/:id' element={<Products />} />
+					<Route path='/product/:id' element={<Product />} />
+				</Route>
+			</Routes>
+		</div>
+	)
 }
 
 export default App
